2dfd3db49ace9f68e8184afffeff6988b279c1b6
alex <alex_kobilianskiy@bigmir.net>
2019-11-27 13:55:09 +0200

    header complete

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,46 @@
 $(document).ready(function(){
 
+    /**
+     * mobile-mnu customization
+     */
+    var mmenu = $('#mobile-mnu');
+    var menuLogo = mmenu.data("logo");
+    var $mmenu = mmenu.mmenu({
+        "pageScroll": true,
+
+        "navbar": {
+            "title" : "Поддоны <br> Ленинградской области <span>Производство поддонов на заказ</span>",
+        },
+        "extensions": [
+            "theme-dark",
+            "pagedim-black",
+            "position-front",
+            "fx-listitems-slide",
+        ],
+    }, {
+        offCanvas: {
+            pageSelector: "#page-container"
+        },
+    });
+
+    var mmenuBtn = $("#mmenu-btn");
+    var API = $mmenu.data("mmenu");
+
+    mmenuBtn.click(function() {
+        API.open();
+        $(this).addClass('is-active')
+    });
+
+
+    API.bind( "close:start", function() {
+        setTimeout(function() {
+            mmenuBtn.removeClass( "is-active" );
+        }, 300);
+    });
+    /**
+     * end mobile-mnu customization
+     */
+
     //E-mail Ajax Send
     $("form").submit(function() { //Change
         var th = $(this);
